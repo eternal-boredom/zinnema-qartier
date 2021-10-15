a9199611dfb38a1e65c4e5e60d2f9c6c0de8b1de
Alexandre Ntougas <alexandrentougas@gmail.com>
2021-10-15 16:17:58 +0200

    carousel automation

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
+let time;
+let highestModalIndex = document.querySelectorAll(".modal").length - 1;
+let nextToPlay = 0;
+let globalIdle;
+let idleVideoTimeout;
+let idleCarouselTimeout;
+let idleCarouselInterval;
+
 window.addEventListener("load", () => {
     initMaterialize();
-    // idlePlay();
+    interruptAutomation();
+    idleCarouselPlay();
 });
+
+function interruptAutomation() {  
+    document.addEventListener("touchstart", () => {
+        suspendIdleCarouselPlay();
+        suspendIdleVideoPlay();
+        
+        // clearTimeout(globalIdle);
+        // globalIdle = setTimeout(idleCarouselPlay, 5000);
+    });
+}
 
 // window.addEventListener("contextmenu", e => e.preventDefault());
 
-let highestModalIndex = document.querySelectorAll(".modal").length - 1;
-let nextToPlay = 0;
-let idleTimeout;
 
 function initMaterialize() {
     var modals = document.querySelectorAll('.modal');
     var materializedModals = M.Modal.init(modals, {
         onOpenStart: function() {
-            suspendIdlePlay();
+            suspendIdleVideoPlay();
+            suspendIdleCarouselPlay();
+            hideHands();
         },
         onOpenEnd: function() {
             this.el.querySelector('video').play();
@@ -23,7 +41,8 @@ function initMaterialize() {
         },
         onCloseEnd: function() {
             this.el.querySelector('video').load();
-            // idlePlay();
+            showHands();
+            idleCarouselPlay();
         }
     });
 
@@ -34,10 +53,14 @@ function initMaterialize() {
     let materializedCarousels = M.Carousel.init(carousel, carouselOptions);
 
     document.querySelector("#left_arrow").addEventListener("click", (event) => {
+        suspendIdleVideoPlay();
+        suspendIdleCarouselPlay();
         materializedCarousels[0].prev();
     })
 
     document.querySelector("#right_arrow").addEventListener("click", (event) => {
+        suspendIdleVideoPlay();
+        suspendIdleCarouselPlay();
         materializedCarousels[0].next();
     })
 }
@@ -51,8 +74,8 @@ videos.forEach((video) => {
     }))
 });
 
-function idlePlay() {
-    idleTimeout = setTimeout(() => {
+function idleVideoPlay() {
+    idleVideoTimeout = setTimeout(() => {
         let instance = M.Modal.getInstance(document.querySelectorAll(".modal")[nextToPlay]);
         instance.open();
         if (nextToPlay < highestModalIndex) {
@@ -60,10 +83,38 @@ function idlePlay() {
         } else {
             nextToPlay = 0;
         }
-    }, (10 * 1000))  // 5 fois 60 secondes (1000 ms)
+    }, (10 * 1000))  // 10 secondes (de 1000 ms)
 }
 
-function suspendIdlePlay() {
-    clearTimeout(idleTimeout);
+function idleCarouselPlay() {
+    idleCarouselTimeout = setTimeout(() => {
+        let rotationCount = 0;
+        let instance = M.Carousel.getInstance(document.querySelector("#carousel"));
+        idleCarouselInterval = setInterval(() => {
+            instance.next();
+            if (++rotationCount === 19) {
+                idleVideoPlay();
+                suspendIdleCarouselPlay();
+            }
+        }, 1000)
+    }, (5 * 1000))  // 20 secondes (de 1000 ms)
 }
 
+function suspendIdleVideoPlay() {
+    clearTimeout(idleVideoTimeout);
+}
+
+function suspendIdleCarouselPlay() {
+    clearTimeout(idleCarouselTimeout);
+    clearInterval(idleCarouselInterval);
+}
+
+function hideHands() {
+    const hands = document.querySelectorAll(".hand");
+    hands.forEach((hand) => {hand.style.display = "none"});
+}
+
+function showHands() {
+    const hands = document.querySelectorAll(".hand");
+    hands.forEach((hand) => {hand.style.display = "block"});
+}
